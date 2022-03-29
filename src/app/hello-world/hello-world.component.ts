de10a2b069856c36f742c0016d6aa0cdc6522f45
frontend200tb <frontend200tb@gmail.com>
2022-03-29 23:16:23 +0300

    init: start my first app

---
+++ src/app/hello-world/hello-world.component.ts
@@ -0,0 +1,13 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hello-world',
+  template: `
+    <h2>My first Angular component</h2>
+    <p>This is only html in template</p>
+  `
+})
+export class HelloWorldComponent {
+  // The code in this class drives the component's behavior.
+}
